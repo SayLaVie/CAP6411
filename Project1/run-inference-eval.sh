61f8547d9ce11d54b249813ce584f4ba7367cdb0
SayLaVie <mmccarver27@gmail.com>
2020-09-08 22:50:33 -0400

    reverted from test state

--- Project1/run-inference-eval.sh
+++ Project1/run-inference-eval.sh
@@ -5,22 +5,15 @@
 
 # Displaying power/fan settings requires sudo
 echo "Current power/fan settings:"
-# sudo /usr/sbin/nvpmodel -q
+sudo /usr/sbin/nvpmodel -q
 
-# for i in {1..5}; do
-#     echo "~~~~~~~~"
-#     echo "Run $i"
-#     docker run -it --rm --runtime nvidia --network host \
-#         -v $(pwd)/baseline:/infer  \
-#         -v $(pwd)/models:/infer/models \
-#         -v $(pwd)/data/validation:/infer/validation \
-#         baseline
-#     echo "~~~~~~~~"
-# done
-
-
-docker run -it --rm --runtime nvidia --network host \
-    -v $(pwd)/baseline:/infer  \
-    -v $(pwd)/models:/infer/models \
-    -v $(pwd)/data/validation:/infer/validation \
-    baseline
+for i in {1..5}; do
+    echo "~~~~~~~~"
+    echo "Run $i"
+    docker run -it --rm --runtime nvidia --network host \
+        -v $(pwd)/baseline:/infer  \
+        -v $(pwd)/models:/infer/models \
+        -v $(pwd)/data/validation:/infer/validation \
+        baseline
+    echo "~~~~~~~~"
+done
