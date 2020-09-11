740f2db0d12dd13dfbf6a1e2a0980ff09f91b0ac
Shane <shane@rhoads.com>
2020-09-11 17:16:59 -0400

    Added support for basis decomposition network compression code

--- Project1/baseline/run-eval.py
+++ Project1/baseline/run-eval.py
@@ -1,12 +1,19 @@
 from baselinemodel import build_model
+from basisModel import basisModel
 from datasets import CIFAR100_Training
 import torch
+import time
 
 
-def eval_model(path):
+def eval_model(path, compression_factor=0):
     device = torch.device("cuda")
     model = build_model().to(device)
     model.load_state_dict(torch.load(path))
+
+    if (compression_factor != 0):
+        compressed_model = basisModel(model, True, True, True)
+        compressed_model.update_channels(compression_factor)
+        model = compressed_model.to(device)
 
     # 100x500x64x64x3 Data size (Classes, images, image dimensions)
     dataset = CIFAR100_Training("./validation")
@@ -15,10 +22,9 @@ def eval_model(path):
     correct = 0
     count = 0
     running_loss = 0
+    start = time.perf_counter()
     model.eval()
 
-    import time
-    start = time.perf_counter()
     with torch.no_grad():
         for x, y in data_generator:
             x = x.squeeze().to(device)
@@ -46,10 +52,10 @@ def eval_model(path):
           "  ACC: %5f" % (correct*100.0/count),
           "  LOSS: %6f" % (running_loss / (count+1)))
 
-    print("Total inference time: %5f seconds\n" % runtime,
-          "Frames per second: %5f" % (count/runtime))
+    print("Total inference time: %5f seconds" % runtime,
+          "\nFrames per second: %5f" % (count/runtime))
 
 
 # TODO: parameterize model to evaluate
 if __name__ == "__main__":
-    eval_model("./models/gpu_model")
+    eval_model("./models/gpu_model", 0.8)
