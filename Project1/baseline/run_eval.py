dba36d107bf5eb11b862588b33baf7e8c9996ae6
Shane <shane@rhoads.com>
2020-09-23 16:55:25 -0400

    DCFnet setup

--- Project1/baseline/run_eval.py
+++ Project1/baseline/run_eval.py
@@ -2,35 +2,27 @@ from baselinemodel import build_model, replace_dcf
 from basisModel import basisModel
 from datasets import CIFAR100_Training
 import torch
-from torch2trt import torch2trt
 import time
 from DCF import *
 
-def eval_model(path, compression_factor=0, use_dcf=false, as_trt=False):
+def eval_model(path, compression_factor=0, use_dcf=False, as_trt=False):
     device = torch.device("cuda")
     model = build_model().to(device)
-    model.load_state_dict(torch.load(path))
-    model.eval()
 
-    if (compression_factor != 0 and use_dcf):
+    if (use_dcf):
         replace_dcf(model)
 
-    elif compression_factor != 0:
+    model.load_state_dict(torch.load(path))
+
+    if (compression_factor != 0 and not use_dcf):
         compressed_model = basisModel(model, True, True, True)
         compressed_model.update_channels(compression_factor)
         compressed_model.cuda()
         model = compressed_model
 
-    print("MODEL: ", model)
+    model.eval()
 
-    if as_trt:
-        print("converting to TRT")
-        # torch.Size([4, 3, 64, 64]) is actual input
-        x = torch.rand((4, 3, 224, 224)).cuda()
-        # x = torch.ones((4, 3, 64, 64)).cuda() # errors out
-        model_trt = torch2trt(model, [x], max_batch_size=4)
-        model = model_trt.to(device)
-        print("finished converting to TRT")
+    print("MODEL: ", model)
 
     # 100x500x64x64x3 Data size (Classes, images, image dimensions)
     dataset = CIFAR100_Training("./validation")
@@ -74,5 +66,5 @@ def eval_model(path, compression_factor=0, use_dcf=false, as_trt=False):
 
 # TODO: parameterize model to evaluate
 if __name__ == "__main__":
-    # eval_model("./models/gpu_model", as_trt=True)
-    eval_model("./models/gpu_model")
+    # eval_model("./models/gpu_model")
+    eval_model("./models/dcf_model20", use_dcf=True)
